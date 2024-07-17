24d74df44c753c28a1f6cdb36e42f91d7da66681
nakamura <hrm.nkmr04@gmail.com>
2024-07-18 08:13:16 +0900

    second commit

--- main.js
+++ main.js
@@ -1,26 +1,24 @@
 var num = 15;
 if ((num >= 10) && (num <= 20)) {
-console.log("ANDの条件と一致した処理");
-console.log('10以上かつ20以下です');
+	console.log("ANDの条件と一致した処理");
+	console.log('10以上かつ20以下です');
 } else {
-console.log("ANDの条件と一致しなかった処理");
-console.log(num);
+	console.log("ANDの条件と一致しなかった処理");
+	console.log(num);
 };
-
 var num = 8;
 if ((num <= 10) || (num >= 20)) {
-console.log("ANDの条件と一致した処理");
-console.log('10以下または20以上です');
+	console.log("ANDの条件と一致した処理");
+	console.log('10以下または20以上です');
 } else {
-console.log("ANDの条件と一致しなかった処理");
-console.log(num);
+	console.log("ANDの条件と一致しなかった処理");
+	console.log(num);
 };
-
 var num = 15;
 if (!(num <= 10)) {
-console.log("NOTの条件と一致した処理");
-console.log('10以上です');
+	console.log("NOTの条件と一致した処理");
+	console.log('10以上です');
 } else {
-console.log("NOTの条件と一致しなかった処理");
-console.log(num);
+	console.log("NOTの条件と一致しなかった処理");
+	console.log(num);
 };
